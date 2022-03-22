0b7a2fcdf26d35f2a4991df4b829dae755d9734c
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-22 16:34:13 +0100

    bug: can't scontare

--- main.js
+++ main.js
@@ -10,11 +10,30 @@ console.log("Ho salvato la variabile età: è", age);
 
 //calcoli: let prezzo = ( 0.21 * km)
 let prezzo=chilometri*0.21;
+console.log("Il prezzo non scontato è", prezzo);
 
 //per la percentuale basta fare *0.20 o *0.40 :DD
+let spesa;
+//dichiarata var spesa
 
-if(age<18){
+if (age < 18) {
     let spesa=prezzo*0.80; //sconto del 20%
-}else if (age>65){
-    let spesa=prezzo*0.60 //sconto del 40%
+} else if (age > 65) {
+    let spesa=prezzo*0.60; //sconto del 40%
+} else {
+    spesa=prezzo;
 }
+
+
+console.log("La spesa è:", spesa);
+
+
+
+// if (time < 10) {
+//    greeting = "Good morning";
+//  } else if (time < 20) {
+//    greeting = "Good day";
+//  } else {
+//    greeting = "Good evening";
+//  }
+  
