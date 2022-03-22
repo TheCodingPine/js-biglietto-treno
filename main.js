d671b99409852f2268d9ea98cdd2e29caddc958b
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-22 15:28:08 +0100

    prompt

---
+++ main.js
@@ -0,0 +1,7 @@
+let chilometri=prompt("Quanti chilometri deve percorrere?");
+//voglio trasformarlo in numero da stinga generica
+chilometri=parseInt(chilometri);
+
+let age=prompt("Quanti anni ha?");
+//in integer anche questo
+age=parseInt(age);
