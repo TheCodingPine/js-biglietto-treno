34fffa986cdfa453aa675e094c7e799b7436acf5
Alberto Rosa <alberto.r.93@hotmail.it>
2022-03-22 17:17:07 +0100

    fixed if

--- main.js
+++ main.js
@@ -11,16 +11,15 @@ console.log("Ho salvato la variabile età: è", age);
 //calcoli: let prezzo = ( 0.21 * km)
 let prezzo=chilometri*0.21;
 console.log("Il prezzo non scontato è", prezzo);
-10
-//per la percentuale basta fare *0.20 o *0.40 :DD
-let spesa;
-//dichiarata var spesa
 
+ //per la percentuale basta fare *0.20 o *0.40 :DD
+    let spesa;
+//dichiarata var spesa
 if (age < 18) {
-    let spesa=prezzo*0.80; //sconto del 20%
+    spesa=prezzo*0.80; //sconto del 20%
     console.log("sotto i 18 paghi", spesa);
 } else if (age > 65) {
-    let spesa=prezzo*0.60; //sconto del 40%
+    spesa=prezzo*0.60; //sconto del 40%
     console.log("oltre i 65:", spesa);
 } else {
     spesa=prezzo;
@@ -28,20 +27,20 @@ if (age < 18) {
 
 
 console.log("La spesa è:", spesa);
-
+// sarà da fixare sui decimali
 
 
 //printa in html
-document.getElementById("output").innerHTML= spesa;
+// document.getElementById("output").innerHTML= spesa;
 
 
 
 
-// if (time < 10) {
-//    greeting = "Good morning";
-//  } else if (time < 20) {
-//    greeting = "Good day";
-//  } else {
-//    greeting = "Good evening";
-//  }
-  
+/* if (time < 10) {
+   greeting = "Good morning";
+  } else if (time < 20) {
+    greeting = "Good day";
+  } else {
+    greeting = "Good evening";
+  }
+  */
